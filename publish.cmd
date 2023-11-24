87f2dd9d72bce8b43af41589c82584a06c847e61
Jonathan Wentz <jrwentz@elantis.net>
2023-11-23 22:04:31 -0500

    Updated the publish batch file

--- publish.cmd
+++ publish.cmd
@@ -1,1 +1,1 @@
-dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish .\ClipboardLinkSaver\ClipboardLinkSaver.csproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -o C:\Utils\ClipboardLinkSaver -p:IncludeNativeLibrariesForSelfExtract=true
